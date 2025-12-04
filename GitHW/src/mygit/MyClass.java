6b5d7e987dabe7c8264e8aa41af55b12116a880b
admin <admin@DESKTOP-I6HUGBR>
2025-12-04 14:33:03 +0900

    Merge branch 'dev'

--- GitHW/src/mygit/MyClass.java
+++ GitHW/src/mygit/MyClass.java
@@ -3,12 +3,12 @@ package mygit;
 public class MyClass {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
-<<<<<<< HEAD
+
 		System.out.println("Hello Git!");
 		System.out.println("My Changed in master branch");
-=======
+
 		System.out.println("Hello Git!2");
 		System.out.println("Hello Git!3");
->>>>>>> refs/heads/dev
+
 	}
 }
