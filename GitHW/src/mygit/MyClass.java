d65019b7c3487e2034fe3f8622bdcea2d61b01e4
admin <admin@DESKTOP-I6HUGBR>
2025-12-04 14:03:22 +0900

    2nd version

--- GitHW/src/mygit/MyClass.java
+++ GitHW/src/mygit/MyClass.java
@@ -1,7 +1,8 @@
 package mygit;
 
 public class MyClass {
-public static void main(String[] args) {
- System.out.println("Hello Git!");
- }
- }
+	public static void main(String[] args) {
+		System.out.println("Hello Git!");
+		System.out.println("Hello Git!");
+	}
+}
